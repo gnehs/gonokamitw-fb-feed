b1635a4576be9303caadc23482a30e604ad811cb
gnehs <jayuiop@gmail.com>
2023-01-28 15:55:12 +0800

    updated script

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ import puppeteer from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import { executablePath } from 'puppeteer';
 import fs from 'fs';
+import fetch from 'node-fetch';
+import crypto from 'crypto';
+
+function hash(str, salt = 'salt') {
+  const hash = crypto.createHash('sha256')
+  hash.update(str.toString() + salt, 'utf8')
+  return hash.digest('hex').slice(0, 8)
+}
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -10,7 +18,6 @@ const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
   puppeteer.use(StealthPlugin());
 
   const browser = await puppeteer.launch({
-    headless: false,
     executablePath: executablePath(),
     args: [
       '--no-sandbox',
@@ -23,50 +30,66 @@ const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
   });
   const page = await browser.newPage();
 
-  let facebookCredentials = {
-    email: process.env.FB_EMAIL,
-    pass: process.env.FB_PASS
-  }
-
   // Set screen size
   await page.setViewport({ width: 1080, height: 1024 });
-  await page.goto(`https://mbasic.facebook.com/login`, { waitUntil: 'networkidle2' });
-  await page.waitForSelector('input[name="email"]');
-  await page.type('input[name="email"]', facebookCredentials.email);
-  await page.type('input[name="pass"]', facebookCredentials.pass);
-  await page.click('input[name="login"]');
-
-  await page.waitForSelector(`[value="好"]`)
-  await page.click(`[value="好"]`);
-  await delay(1000);
-
-  await page.goto(`https://mbasic.facebook.com/gonokamitw`, { waitUntil: 'networkidle2' });
-  await page.waitForSelector(`a[href^="/gonokamitw?v=timeline"]`);
-  await page.click(`a[href^="/gonokamitw?v=timeline"]`);
-
-  await delay(1000);
-  // get all post
-  const postIds = await page.evaluate(() => {
-    let links = document.querySelectorAll('a[href^="/composer/mbasic/?c_src=share"]');
-    links = Array.from(links).map((link) => link.href).map((href) => {
-      let searchParams = new URL(href).searchParams
-      return { target: searchParams.get('target'), sid: searchParams.get('sid') }
+  console.log('goto page');
+  await page.goto(`https://www.picuki.com/profile/gonokamitw`, { waitUntil: 'networkidle2' });
+  console.log('scroll page');
+  await page.evaluate(async () => {
+    await new Promise((resolve, reject) => {
+      let totalHeight = 0;
+      let distance = 100;
+      let timer = setInterval(() => {
+        let scrollHeight = document.body.scrollHeight;
+        window.scrollBy(0, distance);
+        totalHeight += distance;
+      }, 100);
+      setTimeout(() => {
+        clearInterval(timer);
+        resolve();
+      }, 10000);
     });
-    return links;
   });
+
+  console.log('parse page');
+
   fs.mkdirSync('./dist', { recursive: true });
-  fs.writeFileSync('./dist/postIds.json', JSON.stringify(postIds));
+  fs.mkdirSync('./dist/imgs', { recursive: true });
+  // get all post
+  let posts = await page.evaluate(() => {
+    let posts = document.querySelectorAll('.box-photo');
+    posts = [...posts].map(x => {
+      try {
+        return {
+          img: x.querySelector('.post-image').src,
+          description: x.querySelector('.photo-description').innerHTML.trim(),
+          likes: x.querySelector('.likes_photo').innerText.trim(),
+          time: x.querySelector('.time').innerText.trim(),
+        }
+      } catch (e) {
+        return null
+      }
+    }).filter(x => x !== null)
+    return posts
+  });
+  posts = posts.map(async x => {
+    let id = hash(x.description)
+    let imgSrc = x.img;
+    let imgFileName = id + '.jpeg'
+    console.log(`fetch img: ${imgFileName}`)
+    let img = await fetch(imgSrc)
+    img = new Buffer.from(await img.arrayBuffer())
+    fs.writeFile(`./dist/imgs/${imgFileName}`, img, () => { })
 
-  // fetch posts
-  for (let post of postIds) {
-    console.log(`goto https://mbasic.facebook.com/composer/mbasic/?c_src=share&target=${post.target}&sid=${post.sid}`)
-    await page.goto(`https://mbasic.facebook.com/composer/mbasic/?c_src=share&target=${post.target}&sid=${post.sid}`, { waitUntil: 'networkidle2' });
-    await page.waitForSelector(`[name="xc_message"]`);
-  }
-
-
-
-
+    return {
+      id,
+      ...x,
+      img: `/imgs/${imgFileName}`
+    }
+  })
+  posts = await Promise.all(posts)
+  console.log(`posts: ${posts.length}`)
+  fs.writeFileSync('./dist/posts.json', JSON.stringify(posts));
 
   await browser.close();
 })();
